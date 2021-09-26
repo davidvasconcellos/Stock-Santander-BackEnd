9bd4d56ad431ee5eef6122c49c71368cd066b6cc
unknown <davidvasconcellos16@gmail.com>
2021-09-26 12:12:47 -0300

    Repository creation with bootcamp code

---
+++ src/main/java/com/project/bootcamp/exceptions/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.project.bootcamp.exceptions;
+
+import com.project.bootcamp.util.MessageUtils;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(){
+        super(MessageUtils.No_RECORDS_FOUND);
+    }
+}
